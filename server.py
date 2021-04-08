c6a92dda2503d408afff2e49bfd0a199e31ba3a4
vvv-png <75845303+vvv-png@users.noreply.github.com>
2021-04-08 18:30:08 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
-
+import time
+import random
 HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -11,16 +12,36 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+dicte={
+    "who i am?":" the server",
+    "are you stupid?":"yes",
+    "this is cool?":"yes",
+    "are you pretty?":"no",
+    "i am pretty?":"yes",
+    "hiii":"hiii",
+    "friends":"joey",
+    "am i old?":"no",
+    "are you old?":"yes",
+    "this is fun?":"of course"
+}
 class question():
-    def __init__(self, quest, answer):
-        self.quest=quest
-        self.answer=answer
+    def __init__(self):
+        self.quest,self.answer =random.choice(list(dicte.items()))
+        self.self=self.answer,self.quest
+        dicte.pop(self.quest,self.answer)
 
-q1 = question('hi', 'hi').quest
-q2 = question('bye', 'bye').quest
-    
+q1 = question()
+q2=question()
+q3=question()
+q4=question()
+q5=question()
+q6=question()
+q7=question()
+q8=question()
+q9=question()
+q10=question()
+question_list=[q1,q2,q3,q4,q5,q6,q7,q8,q9,q10]
 def handle_client(conn, addr):
-
     print(f"[NEW CONNECTION] {addr} connected.")
     welcome_msg = "Welcome to our game!!!"
     connected = True
@@ -38,14 +59,37 @@ def handle_client(conn, addr):
             elif msg.lower() == 'start':
                 game(conn, addr)
             else:
-                conn.send("Msg received".encode(FORMAT))
-                
+                conn.send("no function".encode(FORMAT))
+
             print(f"[{addr}] {msg}")
-    
+
     conn.close()
 
+def chack(q,player_answer):
+    if q.answer==player_answer:
+        massege="you were correct"
+    else:
+        massege="you were incorrect"
+    return massege
+
 def game(conn, addr):
-    conn.send("hello!!! ".encode(FORMAT))
+    current_time=time.localtime().tm_min
+    print(current_time)
+    conn.send("hello!!!".encode(FORMAT))
+    for i in range(len(question_list)):
+        time.sleep(0.5)
+        ran=random.choice(question_list)
+        question_list.remove(ran)
+        conn.send(ran.quest.encode(FORMAT))
+        player1_answer=conn.recv(1024).decode(FORMAT)
+        massege=chack(ran,player1_answer)
+        conn.send(massege.encode(FORMAT))
+        current_time1=time.localtime().tm_min
+        print(current_time1)
+        if current_time1==current_time+1:
+            timeing=conn.recv(1024).decode(FORMAT)
+            print(timeing)
+            break
 
 def start():
     server.listen()
@@ -54,8 +98,6 @@ def start():
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CINECTION] {threading.activeCount()-1}")
-
-
+        print(f"[ACTIVE CINECTION] {threading.activeCount() - 1}")
 print('[STARTING] server is starting...')
 start()
