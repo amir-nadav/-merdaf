59b5cca01237d6909380b8ba006a1ed293e96303
amir-nadav <79632472+amir-nadav@users.noreply.github.com>
2021-04-11 10:45:43 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import time
+
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
@@ -21,27 +22,20 @@ def send(msg):
     re = client.recv(2048).decode(FORMAT)
     print(re)
 
+
 msg = input('to start, type start. to disconnect press * ')
 send(msg)
 if msg == '*':
     send(DISCONNECT_MESSAGE)
 elif msg == "start":
-    current_time1 = time.localtime()
-    print(current_time1)
     while True:
-            re = client.recv(2048).decode(FORMAT)
-            print(re)
-            answer=input("type your answer: ")
-            if answer == "*":
-                send(DISCONNECT_MESSAGE)
-            client.send(answer.encode())
-            re = client.recv(2048).decode(FORMAT)
-            print(re)
-            current_time=time.localtime()
-            current_time2=str(time.localtime())
-            if current_time.tm_min==current_time1.tm_min + 1 and current_time.tm_sec == current_time1.tm_sec:
-                client.send(current_time2.encode())
-                print(current_time2)
-                break
-            else:
-                pass
+        re = client.recv(2048).decode(FORMAT)
+        print(re)
+        time_left = client.recv(1024).decode(FORMAT)
+        print(f"you have {time_left} seconds")
+        answer=input("type your answer: ")
+        if answer == "*":
+            send(DISCONNECT_MESSAGE)
+        client.send(answer.encode())
+        re = client.recv(2048).decode(FORMAT)
+        print(re)
